b55df2054fe4861816851275a97060810fc7ce29
pnk2u <159317511+pnk2u@users.noreply.github.com>
2025-01-05 08:18:06 +0100

    datagen for 1.21.4

--- src/main/java/de/pnku/mstv_mrailv/datagen/MoreRailVariantRecipeGenerator.java
+++ src/main/java/de/pnku/mstv_mrailv/datagen/MoreRailVariantRecipeGenerator.java
@@ -10,6 +10,7 @@ import net.fabricmc.fabric.api.tag.convention.v2.ConventionalItemTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.recipes.*;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
@@ -23,7 +24,7 @@ import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import static de.pnku.mstv_mrailv.init.MrailvBlockInit.*;
 import static de.pnku.mstv_mrailv.init.MrailvTags.RAIL_REDSTONE_TORCHES;
-import static de.pnku.mstv_mtv.init.MtvBlockInit.more_torch_blocks;
+import static de.pnku.mstv_mtv.init.MtvItemInit.more_redstone_torches;
 import static net.fabricmc.fabric.api.resource.conditions.v1.ResourceConditions.allModsLoaded;
 import static org.apache.commons.lang3.math.IEEE754rUtils.min;
 
@@ -32,21 +33,14 @@ public class MoreRailVariantRecipeGenerator extends FabricRecipeProvider {
         super(output, registriesFuture);
     }
 
-    public static final List<Item> more_redstone_torches = new ArrayList<>();
-
 
     // Fragile (only works if list building order is the same)
     public Map<Item, Item> more_redstone_rail_torches(){
         Map<Item, Item> more_redstone_rail_torch_items = new HashMap<>();
-        for (Block torchBlock : more_torch_blocks) {
-            if (torchBlock.getDescriptionId().contains("redstone_torch")) {
-                more_redstone_torches.add(torchBlock.asItem());
-            }
-        }
         for (int i = 0; i < min(more_redstone_torches.size(), more_activator_rail_items.size()); i++) {
-            more_redstone_rail_torch_items.put(more_activator_rail_items.get(i), more_redstone_torches.get(i*2));
+            more_redstone_rail_torch_items.put(more_activator_rail_items.get(i), more_redstone_torches.get(i));
         }
-        //LOGGER.info("rs_t_list: " + more_redstone_torches.size() + " a_r_list: " + more_activator_rail_items.size() + " ar_rst_map: " + more_redstone_rail_torch_items.size());
+        LOGGER.info("rs_t_list: " + more_redstone_torches.size() + " a_r_list: " + more_activator_rail_items.size() + " ar_rst_map: " + more_redstone_rail_torch_items.size());
         return more_redstone_rail_torch_items;
     }
 
@@ -63,7 +57,7 @@ public class MoreRailVariantRecipeGenerator extends FabricRecipeProvider {
                     //LOGGER.info(railItem + " -> (from: map size = " + more_rail_wood_types.size() + ") " + woodType + " -> " + stickVariant);
                     if (more_activator_rail_items.contains(railItem)) {
                         Item baseRailItemA = more_simple_rail_items.get(more_activator_rail_items.indexOf(railItem));
-                        ShapedRecipeBuilder.shaped(BuiltInRegistries.ITEM, RecipeCategory.TRANSPORTATION, railItem, 6)
+                        this.shaped(RecipeCategory.TRANSPORTATION, railItem, 6)
                                 .define('#', RAIL_REDSTONE_TORCHES)
                                 .define('S', stickVariant)
                                 .define('X', ConventionalItemTags.IRON_INGOTS)
@@ -74,9 +68,9 @@ public class MoreRailVariantRecipeGenerator extends FabricRecipeProvider {
                                 .unlockedBy("has_rail", has(baseRailItemA))
                                 .save(withConditions(recipeOutput, ResourceConditions.not(allModsLoaded("quad-mstv-mtv"))));
 
-                        LOGGER.info(railItem + " -> Map: " + more_redstone_rail_torch_items.get(railItem));
+                        LOGGER.info(railItem + " -> Map: " + more_redstone_rail_torch_items.get(railItem) + " out of " + more_redstone_rail_torch_items);
 
-                        ShapedRecipeBuilder.shaped(BuiltInRegistries.ITEM, RecipeCategory.TRANSPORTATION, railItem, 6)
+                        this.shaped(RecipeCategory.TRANSPORTATION, railItem, 6)
                                 .define('#', more_redstone_rail_torch_items.get(railItem))
                                 .define('S', stickVariant)
                                 .define('X', ConventionalItemTags.IRON_INGOTS)
@@ -88,7 +82,7 @@ public class MoreRailVariantRecipeGenerator extends FabricRecipeProvider {
                                 .save(withConditions(recipeOutput, ResourceConditions.allModsLoaded("quad-mstv-mtv")), String.valueOf(RecipeBuilder.getDefaultRecipeId(railItem).withSuffix("_from_mstv_torch_variants")));
                     } else if (more_detector_rail_items.contains(railItem)) {
                         Item baseRailItemD = more_simple_rail_items.get(more_detector_rail_items.indexOf(railItem));
-                        ShapedRecipeBuilder.shaped(BuiltInRegistries.ITEM, RecipeCategory.TRANSPORTATION, railItem, 6)
+                        this.shaped(RecipeCategory.TRANSPORTATION, railItem, 6)
                                 .define('R', Items.REDSTONE)
                                 .define('S', stickVariant)
                                 .define('#', Blocks.STONE_PRESSURE_PLATE)
@@ -101,7 +95,7 @@ public class MoreRailVariantRecipeGenerator extends FabricRecipeProvider {
                                 .save(recipeOutput);
                     } else if (more_powered_rail_items.contains(railItem)) {
                         Item baseRailItemP = more_simple_rail_items.get(more_powered_rail_items.indexOf(railItem));
-                        ShapedRecipeBuilder.shaped(BuiltInRegistries.ITEM, RecipeCategory.TRANSPORTATION, railItem, 6)
+                        this.shaped(RecipeCategory.TRANSPORTATION, railItem, 6)
                                 .define('R', Items.REDSTONE)
                                 .define('S', stickVariant)
                                 .define('X', ConventionalItemTags.GOLD_INGOTS)
@@ -113,7 +107,7 @@ public class MoreRailVariantRecipeGenerator extends FabricRecipeProvider {
                                 .save(recipeOutput);
                     }
                     if (more_simple_rail_items.contains(railItem)) {
-                        ShapedRecipeBuilder.shaped(BuiltInRegistries.ITEM, RecipeCategory.TRANSPORTATION, railItem, 16)
+                        this.shaped(RecipeCategory.TRANSPORTATION, railItem, 16)
                                 .define('S', stickVariant)
                                 .define('X', ConventionalItemTags.IRON_INGOTS)
                                 .pattern("X X")
